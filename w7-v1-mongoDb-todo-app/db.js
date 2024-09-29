f88897f1c4725bea64bb60071c4404e8f967d5c3
Ketan Kauntia <ketankauntia26@gmail.com>
2024-09-30 01:38:45 +0530

    created the schema,module & signup endpoint

--- w7-v1-mongoDb-todo-app/db.js
+++ w7-v1-mongoDb-todo-app/db.js
@@ -1,1 +1,29 @@
 const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const objectId = mongoose.objectId;
+
+//schema : is the structure of the data i want to put in my db's collection
+const User = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  name: String,
+});
+
+const Todo = new mongoose.Schema({
+  title: String,
+  done: Boolean,
+  userId: objectId, // every entery/object in the collection/table of db is given a unique object id.
+  // --------------- so we are referencing that unique id to get which user it is.
+});
+
+// model : lets us put the data into the specific table/collection
+// models can be exported as well
+
+const userModel = mongoose.model("users", User); // mongoose.model( name of the collection/table, data to insert )
+const todoModel = mongoose.model("todos", Todo);
+
+//exporting the user and todo "OBJECT"
+module.exports = {
+  userModel: userModel,
+  todoModel: todoModel,
+};
--- w7-v1-mongoDb-todo-app/index.js
+++ w7-v1-mongoDb-todo-app/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
 
+//acessing the todo and user model exported from db.js
+const { userModel, todoModel } = require("./db.js");
+
 const app = express();
 app.use(express.json());
 
-app.post("/signup", function (req, res) {});
+app.post("/signup", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  await userModel.insert({
+    //this will give me a promise since it will be an asynchronous fucntion as it will talk to a server in bahrain or some other country and then return me the task done. SO, we shoudls await it.
+    name: "Ketan",
+    pass: "1234",
+    email: "kk@gmail.com",
+  });
+
+  res.json({
+    msg: "You ahve been signed up succesfully",
+  });
+});
 
 app.post("/signin", function (req, res) {});
 
