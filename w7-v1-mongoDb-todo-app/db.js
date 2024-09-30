9f482f5e047f0bc40e65c1a4692f605f5880c0c6
Ketan Kauntia <ketankauntia26@gmail.com>
2024-09-30 13:44:01 +0530

    finshed sign-up & sign-in route

--- w7-v1-mongoDb-todo-app/db.js
+++ w7-v1-mongoDb-todo-app/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const schema = mongoose.Schema;
-const objectId = mongoose.objectId;
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 //schema : is the structure of the data i want to put in my db's collection
 const User = new mongoose.Schema({
@@ -12,7 +12,7 @@ const User = new mongoose.Schema({
 const Todo = new mongoose.Schema({
   title: String,
   done: Boolean,
-  userId: objectId, // every entery/object in the collection/table of db is given a unique object id.
+  userId: ObjectId, // every entery/object in the collection/table of db is given a unique object id.
   // --------------- so we are referencing that unique id to get which user it is.
 });
 
--- w7-v1-mongoDb-todo-app/index.js
+++ w7-v1-mongoDb-todo-app/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "BholaMaharaj";
+const mongoose = require("mongoose");
 
 //acessing the todo and user model exported from db.js
 const { userModel, todoModel } = require("./db.js");
+
+mongoose.connect(
+  "mongodb+srv://ketankauntia26:kUpZGqnJKCTE4m1Y@cluster0.jz9ux.mongodb.net/w7-ketan-todo"
+);
 
 const app = express();
 app.use(express.json());
@@ -11,19 +18,46 @@ app.post("/signup", async function (req, res) {
   const password = req.body.password;
   const name = req.body.name;
 
-  await userModel.insert({
+  await userModel.create({
     //this will give me a promise since it will be an asynchronous fucntion as it will talk to a server in bahrain or some other country and then return me the task done. SO, we shoudls await it.
-    name: "Ketan",
-    pass: "1234",
-    email: "kk@gmail.com",
+    name: name,
+    password: password,
+    email: email,
   });
 
   res.json({
-    msg: "You ahve been signed up succesfully",
+    msg: "You have been signed up succesfully",
   });
 });
 
-app.post("/signin", function (req, res) {});
+app.post("/signin", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const User = await userModel.findOne({
+    //since it will hit the db, so we need to wait till we get the user is present or not in the db or are teh credentials valid or not.
+    email: email,
+    password: password,
+  });
+
+  console.log(User);
+
+  if (User) {
+    const token = jwt.sign(
+      {
+        user: User._id, // _id is a unique objectId assigned by mongodb whenever an value is inserted.
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token: token,
+    });
+  } else {
+    res.send(403).json({
+      message: "Invalid Credentials",
+    });
+  }
+});
 
 app.post("/todo", function (req, res) {});
 
