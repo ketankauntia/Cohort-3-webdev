a4a0336c279a5f5b882b564a000b28395a9b72d2
Ketan Kauntia <ketankauntia26@gmail.com>
2024-10-04 16:11:56 +0530

    added users route skeleton

--- w8-v1-v2-backend-course-selling-website/index.js
+++ w8-v1-v2-backend-course-selling-website/index.js
@@ -9,4 +9,28 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/user/signup", function (req, res) {
+  res.json({
+    msg: "Signed up succesfully",
+  });
+});
+
+app.get("/user/login", function (req, res) {
+  res.json({
+    msg: "Logged in succesfully",
+  });
+});
+
+app.get("/courses/purchase", function (req, res) {
+  res.json({
+    msg: "Hi there",
+  });
+});
+
+app.get("/courses/view", function (req, res) {
+  res.json({
+    msg: "Hi there",
+  });
+});
+
 app.listen(3000);
