656693b129625180d1e56ed4a20fa3b816d0f513
Ketan Kauntia <ketankauntia26@gmail.com>
2024-10-11 23:21:17 +0530

    w9-react

---
+++ w9-v2-react-project/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <b>Hi there</b>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+function Counter() {
+  return (
+    <div>
+      <h1>1</h1>
+      <button>Increase Count</button>
+    </div>
+  );
+}
+export default App;
