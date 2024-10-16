a74ec72b2557ff378bab43ee4dd48c211450acc3
Ketan Kauntia <ketankauntia26@gmail.com>
2024-10-16 19:59:09 +0530

    changes

--- w9-v2-react-project/src/App.jsx
+++ w9-v2-react-project/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div>
@@ -8,10 +10,26 @@ function App() {
 }
 
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
+
   return (
     <div>
-      <h1>1</h1>
-      <button>Increase Count</button>
+      <h1 id="text">{count}</h1>
+      <button onClick={increaseCount}>Increase Count</button>
+      <button onClick={decreaseCount}>Decrease Count</button>
+      <button onClick={resetCount}>Reset Count</button>
     </div>
   );
 }
