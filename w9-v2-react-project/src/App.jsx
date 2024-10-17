2d9d58ee526df299a29027ad452c82711c139712
Ketan Kauntia <ketankauntia26@gmail.com>
2024-10-17 23:58:21 +0530

    next parts

--- w9-v2-react-project/src/App.jsx
+++ w9-v2-react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -23,6 +23,17 @@ function Counter() {
   function resetCount() {
     setCount(0);
   }
+
+  //this will be logged as many times as the component is re-rendered
+  console.log("re-rendering counter");
+
+  //this will get executed only once when its mounted and will avoid itself on re-rendering's
+  useEffect(function () {
+    setInterval(function () {
+      setCount((count) => count + 1);
+    }, 1000);
+    console.log("Mounted");
+  }, []);
 
   return (
     <div>
