f12c6989f2e36bbbc8bd9c85abae16bed64458ba
Ketan Kauntia <ketankauntia26@gmail.com>
2024-10-25 20:42:34 +0530

    init

--- w10-v2-contextApi-stateRolling/src/App.jsx
+++ w10-v2-contextApi-stateRolling/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <LightBulb />;
 }
 
-export default App
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true);
+
+  return (
+    <div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+    </div>
+  );
+}
+
+function BulbState({ bulbOn }) {
+  return <div>{bulbOn ? "Bulb On" : "Bulb Off"}</div>;
+}
+
+function ToggleBulbState({ setBulbOn }) {
+  function toggleBulb() {
+    setBulbOn((toggleState) => !toggleState);
+  }
+
+  return (
+    <div>
+      <button onClick={toggleBulb}>Toggle bulb state</button>
+    </div>
+  );
+}
+export default App;
