aa71aae3ad01cbbbd2b33fd773a882b2e3b8595c
Ketan Kauntia <ketankauntia26@gmail.com>
2024-11-05 23:35:14 +0530

    major changes- cloned and finished

--- qs-assignment/src/App.js
+++ qs-assignment/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Canvas from "./components/Canvas";
 import Navbar from "./components/Navbar";
 
