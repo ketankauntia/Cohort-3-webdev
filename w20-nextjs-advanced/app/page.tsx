6f3857bbbd1f3f5cc6a4b3059aa6647dcaeefa66
Ketan Kauntia <ketankauntia26@gmail.com>
2025-05-10 23:50:19 +0530

    sessions provider + credentials

--- w20-nextjs-advanced/app/page.tsx
+++ w20-nextjs-advanced/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react"
+
 
 export default function Home() {
   return (
-    <div>Hi there</div>
+    <SessionProvider>
+      <RealHome />
+    </SessionProvider>
   );
+}
+
+function RealHome(){
+  const session = useSession();
+
+  return <div>
+    {session.status === 'authenticated' && <button onClick={()=>signOut()}>Logout</button>}
+    {session.status === 'unauthenticated' && <button onClick={()=>signIn()}>SignIn</button>}
+  </div>
 }
