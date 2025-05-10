c39c8ac5f228db412d1fae6a2978257489cdbd65
Ketan Kauntia <ketankauntia26@gmail.com>
2025-05-11 01:08:26 +0530

    w20: implemented nextAuth - Credential Provider (working)

---
+++ w20-v2-part2/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import NextAuth from "next-auth";
+import CredentialsProvider  from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Email", // this is : login with what? -> for us it's email
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "random@random.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+       
+        //hardcoded a user and returned it.
+        return {
+            username: "qwerty",
+            id:1,
+            email: "qwerty@gmail.com"
+        }
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
