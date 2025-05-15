946c527c0395f2ef015fa44d471a24bfa6b8d49f
Ketan Kauntia <ketankauntia26@gmail.com>
2025-05-16 01:18:11 +0530

    temp auth done for http server

--- w22-excalidraw/apps/http-backend/src/index.ts
+++ w22-excalidraw/apps/http-backend/src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
@@ -6,6 +9,29 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.post("/signup", (req,res)=>{
+    //db call
+    res.json({
+        userId:"123"
+    })
+})
+
+app.post("/signin", (req,res)=>{
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET)
+
+    res.json(token);
+})
+
+app.post("/create-room", middleware, (req,res)=>{
+    res.json({
+        roomId:123
+    });
+})
+
+app.listen(3001, () => {
     console.log("Server is running on port 3000");
 });
